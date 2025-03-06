69adac70dc14029314d35501ab51e52fc00eabf3
notduyly <162235323+notduyly@users.noreply.github.com>
2025-03-06 11:53:58 -0800

    Implement masking

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import cv2
+from util import get_limits
 
 webcam = cv2.VideoCapture(0)
+color = [0, 255, 255] # Yellow in BGR
 
 while True:
     ret, frame = webcam.read()
 
-    cv2.imshow('app', frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_limit, upper_limit = get_limits(color)
+    mask = cv2.inRange(hsv, lower_limit, upper_limit)
+
+    cv2.imshow('frame', mask)
 
     # Check if the kqy 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- util.py
+++ util.py
@@ -3,17 +3,16 @@ import cv2
 
 def get_limits(color):
 
+    # 1x1 NumPy arr with given color(BGR)
     c = np.uint8([[color]])
 
-    # Convert the input color to grayscale
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2GRAY) # hsv = Hue, Saturation, Value
+    # Convert the input color to HSV
+    hsv = cv2.cvtColor(c, cv2.COLOR_BGR2HSV) # hsv = Hue, Saturation, Value
+    hue = hsv[0][0][0]
 
-    # Sets the range for hue (intensity) to isolate certain colors
-    lower_limit = hsvC[0][0][0] - 10, 100, 100
-    upper_limit = hsvC[0][0][0] + 10, 255, 255
-    
-    # Convert both limits back to NumPy array
-    lower_limit = np.array(lower_limit, dtype=np.uint8)
-    upper_limit = np.array(upper_limit, dtype=np.uint8)
+    # Sets the range for hue (intensity) to isolate certain colors.
+    # Then, convert both limits back to NumPy array
+    lower_limit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+    upper_limit = np.array([hue + 10, 255, 255], dtype=np.uint8)
 
     return lower_limit, upper_limit
